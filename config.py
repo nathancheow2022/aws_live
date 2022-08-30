310d842deb2f4d2bb83ac49cd9c13e4ddce655c0
nathancheow2022 <112464650+nathancheow2022@users.noreply.github.com>
2022-08-30 14:19:03 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cgydsnkyetgb.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "cheowheepeng-employee"
+customregion = "us-east-1"
