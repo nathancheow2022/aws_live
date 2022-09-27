fb5d63aff5d62bb41febdb91fb632ac1f27b2619
nathancheow2022 <112464650+nathancheow2022@users.noreply.github.com>
2022-09-27 22:17:14 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -23,7 +23,15 @@ table = 'employee'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('AddEmp.html') #default page
+    return render_template('registration.html') #default page
+
+@app.route("/submit", methods=['GET', 'POST'])
+def submit():
+    return render_template('registration.html')
+
+@app.route("/search", methods=['GET', 'POST'])
+def submit():
+    return render_template('search.html')
 
 
 @app.route("/about", methods=['POST'])
@@ -31,7 +39,7 @@ def about():
     return render_template('www.intellipaat.com')
 
 
-@app.route("/addemp", methods=['POST'])
+@app.route("/addemp", methods=['POST','GET'])
 def AddEmp():
     emp_id = request.form['emp_id']
     first_name = request.form['first_name']
@@ -54,25 +62,6 @@ def AddEmp():
         # Uplaod image file in S3 #
         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
         s3 = boto3.resource('s3')
-
-        try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
-
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location
-
-            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                emp_image_file_name_in_s3)
-
-        except Exception as e:
-            return str(e)
 
     finally:
         cursor.close()
