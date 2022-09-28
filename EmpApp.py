6604484b660ec6032838eae82b823a3ec0c459f9
nathancheow2022 <112464650+nathancheow2022@users.noreply.github.com>
2022-09-28 12:34:50 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -67,7 +67,7 @@ def searchEmp():
     query6 = "SELECT salary FROM employee WHERE emp_id = '{}'".format(emp_id)
     cursor.execute(query6)
     salary = cursor.fetchone()
-    salary = ''.join(salary)
+    salary = ''
 
 
     return render_template('EdtandDeleteEmp.html',first_name = first_name , last_name = last_name,pri_skill = pri_skill,location = location,
