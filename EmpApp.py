a0801b1658bcfbded3dc5c3d6c1eff7287f6087a
nathancheow2022 <112464650+nathancheow2022@users.noreply.github.com>
2022-09-28 23:35:47 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -77,6 +77,35 @@ def backMain():
 
 @app.route("/updateEmp", methods=['POST'])
 def updateEmp():
+    emp_id = request.form.get('emp_id')
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    pri_skill = request.form.get('pri_skill')
+    location = request.form.get('location')
+    salary = request.form.get('salary')
+
+    update_sql = "Update employee set first_name = %s, last_name = %s,pri_skill = %s,location = %s, salary = %s where emp_id = %s"
+    cursor = db_conn.cursor()
+
+    cursor.execute(update_sql, (first_name, last_name, pri_skill, location, salary,emp_id))
+    db_conn.commit()
+
+    return render_template('search.html')
+
+@app.route("/deleteEmp", methods=['POST'])
+def deleteEmp():
+    emp_id = request.form.get('emp_id')
+    
+    delete_sql = "Delete from employee where emp_id = %s"
+    cursor = db_conn.cursor()
+
+    cursor.execute(delete_sql, (emp_id))
+    db_conn.commit()
+
+    return render_template('search.html')
+
+@app.route("/addemp", methods=['POST','GET'])
+def AddEmp():
     emp_id = request.form['emp_id']
     first_name = request.form['first_name']
     last_name = request.form['last_name']
@@ -99,32 +128,13 @@ def updateEmp():
         # Uplaod image file in S3 #
         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
         s3 = boto3.resource('s3')
-        
-        try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
 
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location
-
-            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                emp_image_file_name_in_s3)
-
-        except Exception as e:
-            return str(e)
-
-       finally:
+    finally:
         cursor.close()
 
-       print("all modification done...")
-       return render_template('registration.html')
+    print("all modification done...")
+    return render_template('registration.html')
 
 
-            if __name__ == '__main__':
-                app.run(host='0.0.0.0', port=80, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
