6143b2521ec99fadb310319148298559f23c5d98
nathancheow2022 <112464650+nathancheow2022@users.noreply.github.com>
2022-09-28 13:24:42 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -38,10 +38,6 @@ def searchEmp():
     emp_id = request.form.get('emp_id')
     cursor = db_conn.cursor()
 
-    query = "SELECT emp_id FROM employee WHERE emp_id = '{}'".format(emp_id)
-    cursor.execute(query)
-    emp_id = cursor.fetchone()
-
     query2 = "SELECT first_name FROM employee WHERE emp_id = '{}'".format(emp_id)
     cursor.execute(query2)
     first_name = cursor.fetchone()
@@ -64,6 +60,7 @@ def searchEmp():
     cursor.execute(query6)
     salary = cursor.fetchone()
 
+    db_conn.commit()
 
     return render_template('EdtandDeleteEmp.html',first_name = first_name , last_name = last_name,pri_skill = pri_skill,location = location,
                            salary = salary,emp_id = emp_id)
